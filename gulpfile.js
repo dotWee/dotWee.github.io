c843919591b569e6df13e56749796021d1678db1
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-11-24 08:52:53 +0100

    Move source files to src subdir

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,34 @@ var gulp = require('gulp');
 var minifyCSS = require('gulp-csso');
 
 gulp.task('html', function () {
-    return gulp.src('*.html')
+    return gulp.src('src/*.html')
         .pipe(gulp.dest('dist/'))
 });
 
 gulp.task('css', function () {
-    return gulp.src(['css/*.css', 'node_modules/normalize.css/normalize.css'])
+    return gulp.src(['src/css/*.css', 'node_modules/normalize.css/normalize.css'])
         .pipe(minifyCSS())
         .pipe(gulp.dest('dist/css/'))
 });
 
+gulp.task('img', function () {
+    return gulp.src(['src/img/*.png', 'src/img/*.jpg'])
+        .pipe(gulp.dest('dist/img/'))
+});
+
 gulp.task('js', function () {
-    return gulp.src('js/*.js')
+    return gulp.src('src/js/*.js')
         .pipe(gulp.dest('dist/js/'))
 });
 
 gulp.task('meta', function () {
-    return gulp.src(["browserconfig.xml", "favicon.ico", "humans.txt", "icon.png", "robots.txt", "site.webmanifest"])
+    return gulp.src(['src/browserconfig.xml', 'src/favicon.ico', 'src/humans.txt', 'src/icon.png', 'src/robots.txt', 'src/site.webmanifest'])
         .pipe(gulp.dest('dist/'))
 });
 
 gulp.task('assets', function () {
-    return gulp.src('assets/public.key')
+    return gulp.src('src/assets/public.key')
         .pipe(gulp.dest('dist/assets/'))
 });
 
-gulp.task('default', ['html', 'css', 'js', 'meta', 'assets']);
+gulp.task('default', ['html', 'css', 'img', 'js', 'meta', 'assets']);
