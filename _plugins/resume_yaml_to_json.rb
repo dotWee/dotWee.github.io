29cc0b46c43bfb126c6a262a06a861224bd2af28
Lukas Wolfsteiner <lukas@wolfsteiner.media>
2024-09-11 14:08:46 +0200

    feat: Import additional properties from site configuration to resume.json
    This commit adds the email, url, and summary properties from the site configuration to the basics section of the resume YAML data. It also appends the site baseurl to profile urls and volunteer project urls if they only contain a path. These changes ensure that the resume data is up-to-date with relevant information from the site configuration.

--- _plugins/resume_yaml_to_json.rb
+++ _plugins/resume_yaml_to_json.rb
@@ -1,5 +1,7 @@
 require 'yaml'
 require 'json'
+require 'digest'
+require 'uri'
 
 Jekyll::Hooks.register :site, :post_write do |site|
   yaml_path = File.join(site.source, "_data", "resume.yml")
@@ -7,6 +9,27 @@ Jekyll::Hooks.register :site, :post_write do |site|
 
   if File.exist?(yaml_path)
     yaml_data = YAML.load_file(yaml_path)
+
+    # Add additional properties from the site configuration
+    yaml_data["basics"]["email"] = site.config["email"]
+    yaml_data["basics"]["url"] = site.config["url"]
+    yaml_data["basics"]["summary"] = site.config["description"]
+
+    # Add image property by using the gravatar hash
+    email = site.config["email"].downcase
+    hash = Digest::SHA256.hexdigest(email)
+    yaml_data["basics"]["image"] = "https://www.gravatar.com/avatar/#{hash}?s=600"
+
+    # Append the site baseurl to the profile urls, if it only contains the path
+    yaml_data["basics"]["profiles"].each do |profile|
+      profile["url"] = File.join(site.config["url"], profile["url"]) if profile["url"].start_with?("/")
+    end
+
+    # Same for the volunteer projects
+    yaml_data["volunteer"].each do |volunteer|
+      volunteer["url"] = File.join(site.config["url"], volunteer["url"]) if volunteer["url"].start_with?("/")
+    end
+
     File.open(json_path, "w") do |f|
       f.write(JSON.pretty_generate(yaml_data))
     end
