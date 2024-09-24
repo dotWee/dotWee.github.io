34655380103061c02d157bddf4b964615eceee23
Lukas Wolfsteiner <lukas@wolfsteiner.media>
2024-09-24 13:12:56 +0200

    refactor: Recursive function to append site baseurl to relative paths
    This commit refactors the code in `resume_yaml_to_json.rb` to append the site baseurl to profile and volunteer URLs. Previously, this was done using separate loops for each type of URL. Now, a new function `append_baseurl_to_yaml` is introduced which handles nested structures (hashes and arrays) within the YAML data. It processes all string values in the YAML structure and appends the base URL to any string that appears to be a relative URL path starting with '/'. This change improves code readability and maintainability.
    
    The implications of this change are that all relative URLs in the YAML file will now have the site baseurl prepended to them. This ensures that these URLs are correctly resolved when used on a website. The modified code can be reused in other projects where similar functionality is required.
    
    Example usage:
    ```ruby
    yaml_str = "
    site:
      url: /home
    pages:
      - url: /about
      - url: https://example.com/contact
    "
    baseurl = 'https://mywebsite.com'
    result = append_baseurl_to_yaml(yaml_str, baseurl)
    puts result
    # Output:
    # ---
    # site:
    #   url: https://mywebsite.com/home
    # pages:
    # - url: https://mywebsite.com/about
    # - url: https://example.com/contact
    ```
    
    Note that caution should be exercised when using this function on YAML files that may contain non-URL string values, as it modifies all string values starting with '/'.

--- _plugins/resume_yaml_to_json.rb
+++ _plugins/resume_yaml_to_json.rb
@@ -10,6 +10,9 @@ Jekyll::Hooks.register :site, :post_write do |site|
   if File.exist?(yaml_path)
     yaml_data = YAML.load_file(yaml_path)
 
+    # Append the site baseurl to the profile urls, if it only contains the path
+    yaml_data = append_baseurl_to_yaml(yaml_data, site.config["url"])
+
     # Add additional properties from the site configuration
     yaml_data["basics"]["email"] = site.config["email"]
     yaml_data["basics"]["url"] = site.config["url"]
@@ -17,16 +20,6 @@ Jekyll::Hooks.register :site, :post_write do |site|
 
     # Add image property by using the gravatar hash
     yaml_data["basics"]["image"] = get_gravatar_url(site.config["email"])
-
-    # Append the site baseurl to the profile urls, if it only contains the path
-    yaml_data["basics"]["profiles"].each do |profile|
-      profile["url"] = add_baseurl(site, profile["url"])
-    end
-
-    # Same for the volunteer projects
-    yaml_data["volunteer"].each do |volunteer|
-      volunteer["url"] = add_baseurl(site, volunteer["url"])
-    end
 
     File.open(json_path, "w") do |f|
       f.write(JSON.pretty_generate(yaml_data))
@@ -37,13 +30,59 @@ Jekyll::Hooks.register :site, :post_write do |site|
   end
 end
 
+
+# Appends a base URL to relative URL paths in a YAML string
+#
+# This function takes a YAML string and a base URL, then processes all string
+# values in the YAML data structure. It appends the base URL to any string
+# that appears to be a relative URL path (i.e., start with '/').
+# The function handles nested structures (hashes and arrays) within the YAML.
+#
+# @param yaml_string [String] The input YAML string to be processed
+# @param baseurl [String] The base URL to be prepended to relative paths
+#
+# @return [String] A new YAML string with modified URL paths
+#
+# @example
+#   yaml_str = "
+#   site:
+#     url: /home
+#   pages:
+#     - url: /about
+#     - url: https://example.com/contact
+#   "
+#   baseurl = 'https://mywebsite.com'
+#   result = append_baseurl_to_yaml(yaml_str, baseurl)
+#   puts result
+#   # Output:
+#   # ---
+#   # site:
+#   #   url: https://mywebsite.com/home
+#   # pages:
+#   # - url: https://mywebsite.com/about
+#   # - url: https://example.com/contact
+#
+# @note This function modifies all string values that start with '/'.
+#       Be cautious when using it on YAML that may contain non-URL string values.
+def append_baseurl_to_yaml(yaml_obj, baseurl) # recursive function to process nested structures
+  case yaml_obj
+  when Hash
+    yaml_obj.transform_values! { |v| append_baseurl_to_yaml(v, baseurl) }
+  when Array
+    yaml_obj.map! { |v| append_baseurl_to_yaml(v, baseurl) }
+  when String
+    if yaml_obj.start_with?("/")
+      File.join(baseurl, yaml_obj)
+    else
+      yaml_obj
+    end
+  else
+    yaml_obj
+  end
+end
+
 def get_gravatar_url(email, size=600) # get the gravatar url for the email, with a default size of 600
   email = email.downcase
   hash = Digest::SHA256.hexdigest(email)
   return "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
-end
-
-def add_baseurl(site, url) # add the baseurl to the url if it only contains the path
-  return File.join(site.config["url"], url) if url.start_with?("/")
-  return url
 end
